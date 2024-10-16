46bc89b54700bec9aac3c94c09b7513c5d32e524
mcRobusta <46813617+mcRobusta@users.noreply.github.com>
2024-10-16 07:49:23 +0100

    Update setup.py

--- Serverless Function to gather stock data/setup.py
+++ Serverless Function to gather stock data/setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+"""
+Vercel doesn't like installing new modules via pip (like yfinance),
+so we package yfinance as our own custom module and tell Vercel
+to install it via a local file.
+"""
+
 setup(
     name='my_package',
     version='0.1',
